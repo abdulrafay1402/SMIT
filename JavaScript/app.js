4f09095d97c42ff64a2d4e681b0dc9410b41f921
abdulrafay1402 <abdulrafay14021997@gmail.com>
2025-08-01 15:41:22 +0500

    JavaScript Assighments Added

---
+++ JavaScript/app.js
@@ -0,0 +1,55 @@
+// Assighmrnt 1
+// 1.alert("welcome");
+// 2.alert("plaese enter a valid Password");
+// 3.alert("Welcome to JS Land \n Happy Coding");\
+// 4.alert("Welcome to JS Land");
+//  alert("Prevent this page from creating additional dialogs");
+// 5.alert("Hello i can run Js through my web browser console");
+
+// Assighment 2
+// var username;
+// var myName = "Abdul Rafay";
+// var message = " Hello World";
+// alert(message);
+// var Name =  Ali;
+// var Age = 15;
+// var Dep = "Web and App developement"
+// alert( "Name " + Name );
+// alert(Name + " is " + Age + " years old");
+// alert( Dep);
+// var pizza = "PIZZA";
+// alert(pizza + "\n" + pizza.substring(0, 3)+"\n"+ pizza.substring(0 , 4) + "\n" + pizza.substring(0, 2) + "\n" + pizza.substring(0, 1) + " \n" + pizza.substring(0, 0));
+//  var email = "arafay@gmail.com";
+//  alert( " my email is " + email);
+//  var book = " A smarter way to learn JavaScript";
+//  alert("i am trying to learning book " + book);
+// var message = "Yah! i can write HTML Content through JS";
+// document.writeln(message);
+//  var sym = "----$----";
+//  alert(sym);
+
+
+// Assighment 3 
+// var age = 18;
+// alert("I am " + age + " years old");
+// var num = 10;
+// alert("You have visited site " + num + " times");
+// var birthYear = 2007;
+// document.writeln("I was born in " + birthYear + " \t");
+// document.writeln("<br>");
+// document.writeln("Data type of my assighned variable is " + typeof birthYear);
+var visitorName = "Abdul Rafay";
+var ProductId = 101;
+var UnitPrice = 100;
+var Quantity = 10;
+var Total = UnitPrice * Quantity;
+document.writeln("Name of Visitor is " + visitorName);
+document.writeln("<br>");
+document.writeln("Product Id is " + ProductId);
+document.writeln("<br>");
+document.writeln("Unit Price is " + UnitPrice);
+document.writeln("<br>");
+document.writeln("Quantity is " + Quantity);
+document.writeln("<br>");
+document.writeln("Total is " + Total);
+document.writeln("<br>");
